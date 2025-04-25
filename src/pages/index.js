93d61c9d95efdab9b9689491f916c1edcba45c76
FelipeMoretti <felipemoretti10@gmail.com>
2025-04-25 17:35:06 -0300

    Finalização do .js e do .css da página home

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,17 @@ import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import * as styles from "../styles/index.module.css";
 import logoCompleta from "../images/logo-cafe-sem-filtro-transparent.png";
+import cafeExpresso from "../images/cafe-expresso.png";
+import cafeGelado from "../images/cafe-gelado.jpg";
+import cafeChocolate from "../images/cafe-com-chocolate.jpg";
+import frappe from "../images/frappe.jpg";
+import imagemInformacoes from "../images/lampada-com-folhas.jpg";
+import imagemWorkshop from "../images/fazendo-cafe.jpg"
 
 const HomePage = () => {
   return (
     <>
-      <Navbar pageTitle="Café sem Filtro"></Navbar>
+      <Navbar pageTitle="Café sem Filtro" />
       <main>
         <div className={styles.hero}>
           <div className={styles.conteudoHero}>
@@ -27,14 +33,82 @@ const HomePage = () => {
               </div>
             </div>
             <div className={styles.botoes}>
-              <button id="" className={styles.produtos}>Produtos</button>
-              <button id="" className={styles.experiencia}>Experiência</button>
+              <Link to="/menu">
+              <button id="" className={styles.produtos}>
+                Produtos
+              </button>
+              </Link>
+              <button id="" className={styles.experiencia}>
+                Experiência
+              </button>
             </div>
           </div>
         </div>
-        <div className={styles.maisVendidos}></div>
+        <div className={styles.maisVendidos}>
+          <div className={styles.tituloMaisVendidos}>
+            <h1>Mais Vendidos</h1>
+          </div>
+          <div className={styles.cards}>
+            <div className={styles.item}>
+              <div className={styles.card}>
+                <img src={cafeExpresso} alt="Café expresso" />
+                <p>Café Expresso</p>
+              </div>
+              <button id="" className={styles.botaoComprar}>
+                Comprar
+              </button>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.card}>
+                <img src={cafeGelado} alt="Café gelado" />
+                <p>Café Gelado</p>
+              </div>
+              <button id="" className={styles.botaoComprar}>
+                Comprar
+              </button>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.card}>
+                <img src={cafeChocolate} alt="Café com chocolate" />
+                <p>Café com Chocolate</p>
+              </div>
+              <button id="" className={styles.botaoComprar}>
+                Comprar
+              </button>
+            </div>
+            <div className={styles.item}>
+              <div className={styles.card}>
+                <img src={frappe} alt="Frappe" />
+                <p>Frappe</p>
+              </div>
+              <button id="" className={styles.botaoComprar}>
+                Comprar
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.informacoes}>
+            <img src={imagemInformacoes} alt="Lâmpada com folhas" />
+          <div className={styles.textoInformacoes}>
+          <p>
+            Localização: Rua dos Grãos de Café - CEP 12345-678 <br /><br /> 
+            Horário defuncionamento: 06:00 - 19:00 de Seg a Sáb. <br /><br />
+            Telefone: (11) 99140-7988 - Whatsapp
+          </p>
+          </div>
+        </div>
+        <div className={styles.sobre}>
+          <h1>Sobre Nós</h1>
+          <p>Na nossa cafeteria, cada xícara conta uma história. Usamos grãos selecionados de cultivo sustentável e comércio justo, preparados em métodos especiais como prensa francesa, siphon e cold brew para revelar nuances únicas de sabor. Quer ir além de provar? Participe dos nossos workshops de barista e degustações interativas, aprenda a arte do café e crie sua própria bebida. Venha viver uma experiência que respeita a origem do grão e celebra novos aromas em cada preparo.</p>
+        </div>
+        <div className={styles.workshop}>
+          <div className={styles.textoWorkshop}>
+            <p>Descubra o universo do café nos nossos workshops de barista. Em encontros práticos e descontraídos, você aprende a escolher grãos, dominar métodos como espresso, prensa francesa e cold brew, e a texturizar o leite para latte art de respeito. Saia preparado para criar bebidas incríveis em casa, entendendo cada etapa da moagem ao serviço e elevando sua rotina com cafés que contam história em cada gole.</p>
+          </div>
+          <img src={imagemWorkshop} alt="Homem fazendo café" />
+        </div>
       </main>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 };
